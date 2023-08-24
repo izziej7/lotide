60b5557db8c48213f6f0c8fa838bef520f3c8c8c
Fizza Jafri <jafrifm@gmail.com>
2023-08-24 16:18:08 -0400

    Create and test tail function

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
-// Had to rename assertEqual because VS Code was detecting declaration in other file
-const assertEquals = function(actual, expected) {
+// Use to test head function
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,16 +7,13 @@ const assertEquals = function(actual, expected) {
   }
 };
 
-// Returns the first element (head) of an array
+// Return the first element (head) of an array
 const head = function(array) {
   return array[0];
 };
 
-// Test results
-assertEquals(head([5, 6, 7]), 5);
-assertEquals(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEquals(head([4]), 4);
-assertEquals(head([]), undefined);
-assertEquals(head([3, 4, 5]), 4);
-assertEquals(head([]), 0);
-
+// Test head function
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([4]), 4);
+assertEqual(head([]), undefined);
