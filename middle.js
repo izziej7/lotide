3ac1a985b85fbc1a49374de8b7e73425321eb8e9
Fizza Jafri <jafrifm@gmail.com>
2023-08-25 18:55:38 -0400

    Create and test middle function

---
+++ middle.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  const middleArray = [];
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 !== 0) {
+    return [array[Math.floor(array.length / 2)]];
+  } else {
+    return [array[(array.length / 2) - 1], array[(array.length / 2)]];
+  }
+};
+
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle(["a", "b", "c", "d", "e"]), ["c"]);
