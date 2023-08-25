319cb1e69e612c7fb6b3e118c4b5fe75b3265ed9
Fizza Jafri <jafrifm@gmail.com>
2023-08-25 17:52:35 -0400

    Create and test flatten function

---
+++ flatten.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const flatten = function(nestedArray) {
+  const flattenedArray = [];
+  let index = 0;
+  for (let i = 0; i < nestedArray.length; i++) {
+    if (Array.isArray(nestedArray[i])) {
+      for (let j = 0; j < nestedArray[i].length; j++) {
+        flattenedArray[index] = nestedArray[i][j];
+        index++;
+      }
+    } else {
+      flattenedArray[index] = nestedArray[i];
+      index++;
+    }
+  }
+  return flattenedArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(["1", "2", ["3", "4"], "5", ["6"]]), ["1", "2", "3", "4", "5", "6"]);
+assertArraysEqual(flatten([]), []);
+assertArraysEqual(flatten([1, 2, 3, [], 4]), [1, 2, 3, 4]);
