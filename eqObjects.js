474b0c7dbb57e785308ba832fbcc37ff8915de68
Fizza Jafri <jafrifm@gmail.com>
2023-08-29 17:28:10 -0400

    Create and test eqObjects function for primitive values

---
+++ eqObjects.js
@@ -0,0 +1,32 @@
+// Use to test eqObjects function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Return true if both objects have identical keys with identical values
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key of Object.keys(object1)) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// Test eqObjects function
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
