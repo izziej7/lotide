a9462893b88c07700f6423e68769197be28bfdfd
Fizza Jafri <jafrifm@gmail.com>
2023-09-10 14:55:43 -0400

    [test]: refactor existing test code and add more test code for objects within objects

--- eqObjects.js
+++ eqObjects.js
@@ -39,16 +39,31 @@ const eqObjects = function(actual, expected) {
 };
 
 // Test eqObjects function
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+const shirtOne = { color: "red", size: "medium" };
+const shirtTwo = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtOne, shirtTwo), true);
 
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+const shirtThree = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtOne, shirtThree), false);
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+const shirtFour = { colors: ["red", "blue"], size: "medium" };
+const shirtFive = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(shirtFour, shirtFive), true);
 
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+const shirtSix = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(shirtFour, shirtSix), false);
+
+const objOne = { a: { z: 1 }, b: 2 };
+const objTwo = { a: { z: 1 }, b: 2 };
+assertEqual(eqObjects(objOne, objTwo), true);
+
+const objThree = { a: { y: 0, z: 1 }, b: 2 };
+const objFour = { a: { z: 1 }, b: 2 };
+assertEqual(eqObjects(objThree, objFour), false);
+
+const objFive = { a: { c: { y: 0, d: { z: 1 } } }, b: 2 };
+const objSix = { a: { c: { y: 0, d: { z: 1 } } }, b: 2 };
+assertEqual(eqObjects(objFive, objSix), true);
+
+const objSeven = { a: { c: { y: 0, d: { z: 2 } } }, b: 2 };
+assertEqual(eqObjects(objSix, objSeven), false);
