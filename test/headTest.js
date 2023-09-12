d8b8337ce90588a50b66d8adc1f3872c3af711b5
Fizza Jafri <jafrifm@gmail.com>
2023-09-12 19:17:54 -0400

    [test]: update test code to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,17 @@
 // Use to test the head function
 const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 
 // Test head function
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([4]), 4);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
