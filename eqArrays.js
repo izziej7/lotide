443aa92e1b63557ccb54096d75453f54437a8530
Fizza Jafri <jafrifm@gmail.com>
2023-09-12 18:01:48 -0400

    [refactor]: move test code into separate file and replace test function with require call

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// Use to test eqArrays function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Compare two arrays and return a boolean based on whether they are an exact match
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
@@ -24,14 +15,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-// Test eqArrays function
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-assertEqual(eqArrays([1, [[2, 3], [4, [5]]], [6]], [1, [[2, 3], [4, [5]]], [6]]), true);
-assertEqual(eqArrays([1, [[2, 3], [4, [5]]], [6]], [1, [[2, 3], [4, 5]], [6]]), false);
+module.exports = eqArrays;
