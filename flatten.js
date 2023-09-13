daea19e12a3b1472a35e353dc909c074408402ca
Fizza Jafri <jafrifm@gmail.com>
2023-09-13 15:16:38 -0400

    [update]: add comments to document code

--- flatten.js
+++ flatten.js
@@ -1,6 +1,12 @@
-// Return a flattened version (single-level array) of a nested array
+/* Return a flattened version (single-level array) of a nested array
+ * parameter: nestedArray - array of arrays
+ * returns: single-level array */
 const flatten = function(nestedArray) {
+  // declare an empty flatArray array
   const flatArray = [];
+  /* loop through each element in the nestedArray array
+   * if the element is an array, loop through and push each element to flatArray
+   * if the element is not an array, push each element to flatArray */
   for (const item of nestedArray) {
     if (Array.isArray(item)) {
       for (const val of item) {
@@ -10,6 +16,7 @@ const flatten = function(nestedArray) {
       flatArray.push(item);
     }
   }
+  // return the flatArray array
   return flatArray;
 };
 
--- letterPositions.js
+++ letterPositions.js
@@ -1,6 +1,12 @@
-// Determine the index of each character in a sentence and return the results as an object
+/* Determine the index of each character in a sentence and return the results as an object
+ * parameter: sentence - string
+ * returns: object - [string]: number */
 const letterPositions = function(sentence) {
+  // declare an empty results object
   const results = {};
+  /* loop through each character of the sentence
+   * if it is a key in the results object, push the index onto the array value
+   * if it is not a key in the results object, create it with an array containing the index as the value */
   for (let i = 0; i < sentence.length; i++) {
     if (results[sentence[i]]) {
       results[sentence[i]].push(i);
@@ -8,6 +14,7 @@ const letterPositions = function(sentence) {
       results[sentence[i]] = [i];
     }
   }
+  // return the results object
   return results;
 };
 
