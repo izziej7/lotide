b38b9e572d69b0c7c0e900a815d2af1844ef449c
Fizza Jafri <jafrifm@gmail.com>
2023-09-13 13:12:31 -0400

    [test]: update test code to use mocha and chai

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,13 +1,16 @@
 // Use to test countLetters function
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const countLetters = require("../countLetters");
 
 // Test countLetters function
-const result1 = countLetters("code here");
-
-assertEqual(result1["c"], 1);
-assertEqual(result1["o"], 1);
-assertEqual(result1["d"], 1);
-assertEqual(result1["e"], 3);
-assertEqual(result1["h"], 1);
-assertEqual(result1["r"], 1);
+describe("#countLetters", () => {
+  it("returns { 'c': 1, 'o': 1, 'd': 1, 'e': 3, 'h': 1, 'r': 1 } for 'code here'", () => {
+    assert.deepEqual(countLetters("code here"), { 'c': 1, 'o': 1, 'd': 1, 'e': 3, 'h': 1, 'r': 1 });
+  });
+  it("returns { 'b': 1, 'a': 1, 't': 2, 'l': 1, 'e': 1 } for 'battle'", () => {
+    assert.deepEqual(countLetters("beetle"), { 'b': 1, 'e': 3, 't': 1, 'l': 1 });
+  });
+  it("returns {} for an empty string", () => {
+    assert.deepEqual(countLetters(""), {});
+  });
+})
