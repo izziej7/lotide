a53ff23b415f64ae2b93768db33d979f6e2fe085
Fizza Jafri <jafrifm@gmail.com>
2023-09-13 15:02:33 -0400

    [update]: add comments to document code

--- findKey.js
+++ findKey.js
@@ -1,5 +1,11 @@
-// Search through an object and return the first key for which the callback returns a truthy value
+/* Search through an object and return the first key for which the callback returns a truthy value
+ * parameter: object - [string]: any
+ * parameter: callback - function
+ * returns: string || undefined */
 const findKey = function(object, callback) {
+  /* loop through the array of object keys
+   * pass the object's value into the callback function
+   * if the result is truthy, return the corresponding key */
   for (const key of Object.keys(object)) {
     if (callback(object[key])) {
       return key;
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,6 +1,11 @@
 /* Search through an object and return the first key for which the value equals parameter value
- * exclude objects that have arrays or objects as values */
+ * exclude objects that have arrays or objects as values 
+ * parameter: object - [string]: any primitive 
+ * parameter: value - any primitive 
+ * returns: string || undefined */
 const findKeyByValue = function(object, value) {
+  /* loop through the array of object keys
+   * if the object's value equals the parameter value, return the corresponding key */
   for (const key of Object.keys(object)) {
     if (object[key] === value) {
       return key;
