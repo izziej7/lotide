64471c956ee8b668f2f6932220d78d7c3652ea84
Fizza Jafri <jafrifm@gmail.com>
2023-09-12 21:26:28 -0400

    [test]: update test code to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,8 @@
 // Use to test the middle function
-const assertArraysEqual = require("../assertArraysEqual");
 const assert = require("chai").assert;
 const middle = require("../middle");
 
 // Test middle function
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle(["a", "b", "c", "d", "e"]), ["c"]);
-
 describe("#middle", () => {
   it("returns [] for [1, 2]", () => {
     assert.deepEqual(middle([1, 2]), []);
@@ -21,5 +13,7 @@ describe("#middle", () => {
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
-
+  it("returns ['c'] for ['a', 'b', 'c', 'd', 'e']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
+  });
 });
