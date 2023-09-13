2a7af2cd4700aee70e45b01317a935d4770f8509
Fizza Jafri <jafrifm@gmail.com>
2023-09-13 13:11:50 -0400

    [update]: add comments to document code

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,13 @@
 /* Count the occurrence of each letter in a sentence and return the results as an object
- * exclude spaces */
+ * exclude spaces 
+ * parameter: sentence - string
+ * returns: object - [string]: number */
 const countLetters = function(sentence) {
+  // declare an empty results object
   const results = {};
+  // loop through each letter of the sentence
+  // if it is not a space and it is a key in the results object, add 1 to the value
+  // if it is not a space and it is not a key in the results object, create it with value = 1
   for (const letter of sentence) {
     if (letter !== " ") {
       if (results[letter]) {
@@ -11,6 +17,7 @@ const countLetters = function(sentence) {
       }
     }
   }
+  // return the results object
   return results;
 };
 
