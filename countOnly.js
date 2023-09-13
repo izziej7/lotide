b6ad0b9c5ca8965a9fb94ae5d36f3f51daafe966
Fizza Jafri <jafrifm@gmail.com>
2023-09-13 13:40:13 -0400

    [update]: add comments to document code

--- countOnly.js
+++ countOnly.js
@@ -1,6 +1,13 @@
-// Count the occurrence of each specified string and return the results as an object
+/* Count the occurrence of each specified string in an array and return the results as an object
+ * parameter: allItems - array
+ * parameter: itemsToCount - object - [string]: boolean
+ * returns: object - [string]: number */
 const countOnly = function(allItems, itemsToCount) {
+  // declare an empty results object
   const results = {};
+  /* loop through each string in the allItems array, if the string is true in itemsToCount and
+   * it is a key in the results object, add 1 to the value
+   * it is not a key in the results object, create it with value = 1 */
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
@@ -10,6 +17,7 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
+  // return the results object
   return results;
 };
 
