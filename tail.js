428fd9983ae01b9259c21aef81f570bab53ef511
Fizza Jafri <jafrifm@gmail.com>
2023-09-14 16:10:00 -0400

    [update]: revise comments for clarity

--- tail.js
+++ tail.js
@@ -1,12 +1,13 @@
 // Returns all except the first element of an array in a new array
 const tail = function(array) {
   const tailArray = [];
-  /* loop through each element of array
-   * start with index 1 (skip the first element)
-   * push each element into new array */
+
+  // loop through each element of array, start with index 1 (skip the first element)
   for (let i = 1; i < array.length; i++) {
+    // push each element into new array
     tailArray.push(array[i]);
   }
+  
   return tailArray;
 };
 
