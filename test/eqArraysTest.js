881ea8829ee04d6cd6765deef782145e04372f3f
Fizza Jafri <jafrifm@gmail.com>
2023-09-16 19:34:04 -0400

    [test]: add test case

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -22,4 +22,7 @@ describe("#eqArrays", () => {
   it("returns false for [1, [[2, 3], [4, [5]]], [6]] and [1, [[2, 3], [4, 5]], [6]]", () => {
     assert.strictEqual(eqArrays([1, [[2, 3], [4, [5]]], [6]], [1, [[2, 3], [4, 5]], [6]]), false);
   });
-})
+  it("returns false for [1, 2, 3] and [1, [2], 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, [2], 3]), false);
+  });
+});
